b8e12d7a1ef3195cc4658f775d3114987af3a75c
Martin Bouchard <marbouchl@gmail.com>
2015-10-13 23:04:21 -0400

    ajout d'une petite optimisation de prediction

--- app/RemotePlayer.js
+++ app/RemotePlayer.js
@@ -1,8 +1,7 @@
-
-var RemotePlayer = function(game,id, x ,y){
-    var self= this;
+var RemotePlayer = function(game, id, x, y) {
+  var self = this;
   this.game = game;
-  this.car = this.game.add.sprite(x, y , 'car');
+  this.car = this.game.add.sprite(x, y, 'car');
   this.game.physics.p2.enable(this.car);
 
   this.id = id;
@@ -17,58 +16,62 @@ var RemotePlayer = function(game,id, x ,y){
 
 };
 
-RemotePlayer.prototype.updateOther = function(){
+RemotePlayer.prototype.updateOther = function() {
+
+
+   if (this.left) {
+     this.car.body.rotateLeft(100);
+   } else if (this.right) {
+    this.car.body.rotateRight(100);
+  } else {
+    this.car.body.setZeroRotation();
+  }
+  if (this.up) {
+    this.car.body.thrust(400);
+  } else if (this.down) {
+    this.car.body.reverse(400);
+  }
+
+  if (this.car.body.x !== this.lastX || this.car.body.y !== this.lastY) {
     this.car.body.x = this.lastX;
     this.car.body.y = this.lastY;
     this.car.body.rotation = this.lastRotation;
-    // if (this.left) {
-    //   this.car.body.rotateLeft(100);
-    // } //ship movement
-
-  //  this.car.rotation = Math.PI + this.game.physics.arcade.angleToXY(this.car, this.lastX, this.lastY);
-    // else if (this.right) {
-    //   this.car.body.rotateRight(100);
-    // } else {
-    //   this.car.body.setZeroRotation();
-    // }
-    // if (this.up) {
-    //   this.car.body.thrust(400);
-    // } else if (this.down) {
-    //   this.car.body.reverse(400);
-    // }
+  }
 
 };
 
-RemotePlayer.prototype.update = function(){
+RemotePlayer.prototype.update = function() {
 
-    this.lastX = this.car.body.x;
-    this.lastY = this.car.body.y;
-    this.lastRotation = this.car.body.rotation;
-    if (this.left) {
-      this.car.body.rotateLeft(100);
-    } //ship movement
-    else if (this.right) {
-      this.car.body.rotateRight(100);
-    } else {
-      this.car.body.setZeroRotation();
-    }
-    if (this.up) {
-      this.car.body.thrust(400);
-    } else if (this.down) {
-      this.car.body.reverse(400);
-    }
+  if (this.left) {
+    this.car.body.rotateLeft(100);
+  } //ship movement
+  else if (this.right) {
+    this.car.body.rotateRight(100);
+  } else {
+    this.car.body.setZeroRotation();
+  }
+  if (this.up) {
+    this.car.body.thrust(400);
+  } else if (this.down) {
+    this.car.body.reverse(400);
+  }
+
+  this.lastX = this.car.body.x;
+  this.lastY = this.car.body.y;
+  this.lastRotation = this.car.body.rotation;
+
+};
+RemotePlayer.prototype.send = function() {
+  return {
+    id: this.id,
+    left: this.left,
+    right: this.right,
+    up: this.up,
+    down: this.down,
+    x: this.car.body.x,
+    y: this.car.body.y,
+    rotation: this.car.body.rotation
   };
-RemotePlayer.prototype.send = function(){
-    return {
-      id: this.id,
-      left: this.left,
-      right: this.right,
-      up: this.up,
-      down: this.down,
-      x: this.car.body.x,
-      y: this.car.body.y,
-      rotation: this.car.body.rotation
-    };
-  };
+};
 
 window.RemotePlayer = RemotePlayer;
